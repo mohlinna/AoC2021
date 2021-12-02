e256274dc020d0986fb2a9ae725296b39221fffc
mohlinna <nathan.mohling@gmail.com>
2021-12-02 01:28:37 -0600

    Restore commented out sections

--- aoc2021/src/aoc2021/core.clj
+++ aoc2021/src/aoc2021/core.clj
@@ -7,10 +7,10 @@
   "Advent of Code 2021"
   [& args]
   (println "==============" "AOC2021 Day 1" "==============")
-  ;(time (day1/part1))
-  ;(time (day1/part2))
+  (time (day1/part1))
+  (time (day1/part2))
   (println "==============" "AOC2021 Day 2" "==============")
-  ;(time (day2/part1))
+  (time (day2/part1))
   (time (day2/part2))
   (println))
   
